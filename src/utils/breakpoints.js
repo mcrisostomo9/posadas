a0228f34c6858bfa8b20618d5be686d33a029752
Mark Crisostomo <mark@deviseinteractive.com>
2019-02-25 16:27:53 -0800

    first commit

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,13 @@
+const mq = {
+    a400: `@media(min-width: 400px)`,
+    a500: `@media(min-width: 500px)`,
+    a768: `@media(min-width: 768px)`,
+    a992: `@media(min-width: 992px)`,
+    a1200: `@media(min-width: 1200px)`,
+    a1400: `@media(min-width: 1400px)`,
+    a1600: `@media(min-width: 1600px)`,
+    a1800: `@media(min-width: 1800px)`,
+    a2200: `@media(min-width: 2200px)`,
+};
+
+export default mq;
