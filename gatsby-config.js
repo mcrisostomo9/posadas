874298b5fada88032889a868121a7cf7152dac49
Mark Crisostomo <mark@deviseinteractive.com>
2019-02-28 18:01:01 -0800

    updated meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `This is Posadas`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `This Website is powered by Gatsby the dog`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -27,9 +27,9 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-styled-components'
+    "gatsby-plugin-styled-components",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
