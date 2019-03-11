10da4eac80530c106152c5e436060a0c7675b696
Mark Crisostomo <mark@deviseinteractive.com>
2019-03-11 12:47:55 -0700

    fix for netlify build v2.0

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,14 +6,14 @@
 
 // You can delete this file if you're not using it
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
   if (stage === "build-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
           {
-            test: /bad-module/,
-            use: loaders.null(),
+            test: /@typeform/,
+            loader: "null-loader",
           },
         ],
       },
